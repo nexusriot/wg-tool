924f5236b61275ad4f54de972ba2cba1ceafa9d0
Vladislav Ananev <vananyev@hystax.com>
2022-03-22 19:30:18 +0400

    server config PoC

--- wg-tool.py
+++ wg-tool.py
@@ -1,7 +1,16 @@
+import argparse
 import os
 import subprocess
 import ipaddress
 from config import *
+
+
+class ServerConfigException(Exception):
+    pass
+
+
+class ClientConfigException(Exception):
+    pass
 
 
 def forward_rules(ena_dev, wg_dev):
@@ -30,20 +39,42 @@ def create_public_key():
         fh.write(output)
 
 
-def init_server_config(do_forward=True):
+def init_server_config(addr=None, do_forward=True):
+    if not ENDPOINT:
+        raise ServerConfigException("Empty endpoint ip")
     config_file = os.path.join(SERVER_CONFIG_BASE_PATH, WG_DEV) + ".conf"
     private_key = get_private_key()
-    net = ipaddress.IPv4Network(NETWORK)
-    first_addr = net[1]  # get the first addr for server config
+    pref_len = 24  # by default 24
+    if not addr:
+        net = ipaddress.IPv4Network(NETWORK)
+        addr = net[1]  # get the first addr for server config
+        pref_len = net.prefixlen
     with open(config_file, "w") as fh:
         fh.write("[Interface]\n")
         fh.write("PrivateKey = {private_key}\n".format(private_key=private_key))
         fh.write("ListenPort = {listen_port}\n".format(listen_port=LISTEN_PORT))
-        fh.write("Address = {first_addr}/{pref_len}\n".format(first_addr=first_addr, pref_len=net.prefixlen))
+        fh.write("Address = {first_addr}/{pref_len}\n".format(first_addr=addr, pref_len=pref_len))
         fh.write("SaveConfig = true\n")
         if do_forward:
             fh.write(forward_rules(ENA_DEV, WG_DEV))
 
 
+def init_client_config():
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='command')
+    config_server = subparser.add_parser('server')
+    config_client = subparser.add_parser('client')
+    config_server.add_argument('--addr', type=str, required=False, default=None)
+    config_client.add_argument('--client_addr', type=str, required=False, default=None)
+
+    args = parser.parse_args()
+    if args.command == 'server':
+        init_server_config(args.addr)
+
+
 if __name__ == "__main__":
-    init_server_config()
+    main()
