155b3d2f1436b46594b83f6f30d32f6384204a51
Vladislav Ananev <vananyev@hystax.com>
2022-03-22 23:14:21 +0400

    added client config

--- wg-tool.py
+++ wg-tool.py
@@ -32,13 +32,18 @@ def get_private_key():
         return fh.read()
 
 
+def get_public_key():
+    with open(PUBLIC_KEY, "r") as fh:
+        return fh.read()
+
+
 def create_public_key():
     private_key = get_private_key()
     ps = subprocess.Popen(('echo', private_key), stdout=subprocess.PIPE)
-    output = subprocess.check_output(("wg", "pubkey"), stdin=ps.stdout).decode().strip()
+    public_key = subprocess.check_output(("wg", "pubkey"), stdin=ps.stdout).decode().strip()
     ps.wait()
     with open(PUBLIC_KEY, "w") as fh:
-        fh.write(output)
+        fh.write(public_key)
 
 
 def init_server_config(addr=None, do_forward=True):
@@ -65,8 +70,29 @@ def init_server_config(addr=None, do_forward=True):
     subprocess.check_call(["systemctl", "start", "wg-quick@wg0.service"])
 
 
-def init_client_config():
-    pass
+def add_peer(client_public_key, client_addr):
+    subprocess.check_output(["wg", "set",  WG_DEV, "peer", client_public_key, "allowed-ips", client_addr])
+
+
+def init_client_config(name, client_addr):
+    if not os.path.isfile(PUBLIC_KEY) or not os.path.isfile(PRIVATE_KEY):
+        raise ClientConfigException("please configure server keys first")
+    server_public_key = get_public_key()
+    client_private_key = subprocess.check_output(["wg", "genkey"]).decode().strip()
+    ps = subprocess.Popen(('echo', client_private_key), stdout=subprocess.PIPE)
+    client_public_key = subprocess.check_output(("wg", "pubkey"), stdin=ps.stdout).decode().strip()
+    ps.wait()
+    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), name + ".conf")
+    address = "%s/24"
+    with open(config_file, "w") as fh:
+        fh.write("[Interface]\n")
+        fh.write("PrivateKey = {private_key}\n".format(private_key=client_private_key))
+        fh.write("Address = {address}\n\n".format(address=address))
+        fh.write("[Peer]\n")
+        fh.write("PublicKey = {server_public_key}\n".format(server_public_key=server_public_key))
+        fh.write("AllowedIPs = 0.0.0.0/0\n")
+        fh.write("Endpoint = {endpoint_ip}:{endpoint_port}\n".format(endpoint_ip=ENDPOINT, endpoint_port=LISTEN_PORT))
+    add_peer(client_public_key, client_addr)
 
 
 def main():
@@ -74,12 +100,17 @@ def main():
     subparser = parser.add_subparsers(dest='command')
     config_server = subparser.add_parser('server')
     config_client = subparser.add_parser('client')
-    config_server.add_argument('--addr', type=str, required=False, default=None)
-    config_client.add_argument('--client_addr', type=str, required=False, default=None)
+    config_server.add_argument('--addr', type=str, required=False, default=None)  # if not set will take first net addr
+    config_client.add_argument('--client_addr', type=str, required=True)
+    config_client.add_argument('--name', type=str, required=True)
 
     args = parser.parse_args()
     if args.command == 'server':
         init_server_config(args.addr)
+    elif args.command == 'client':
+        init_client_config(args.name, args.client_addr)
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
